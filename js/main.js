db6ce49778850da19bef9cb8cbe476faeaf11883
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-11-13 12:19:24 +0100

    create closeNavMenu and openNavMenu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 'use strict';
 
+ const triggerBtn = document.querySelector('.nav-trigger');
+ const nav= document.querySelector('.nav');
+ const closeBtn = document.querySelector('.nav-close');
 
+ console.log('holaaa')
+
+function openNavMenu() {
+  nav.classList.add('nav-open')
+}
+function closeNavMenu() {
+  nav.classList.remove('nav-open')
+}
+
+closeBtn.addEventListener('click',closeNavMenu);
+triggerBtn.addEventListener('click',openNavMenu);
 
